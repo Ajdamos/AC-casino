f24086f9312a04789b6babd124c3b2feebc48b4f
Ajdamos <100752422+Ajdamos@users.noreply.github.com>
2023-04-09 16:27:13 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,5 @@
 "use client"
-import React, { useEffect } from 'react'
-import { useRouter } from "next/navigation";
-
-
 export default function page() {
-
-  const router = useRouter()
-
-  
   return (
     <div>Tohle je práce Adama Černoše</div>
   )
